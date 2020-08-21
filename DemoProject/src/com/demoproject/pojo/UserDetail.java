ddb1bc7235febf7949a8848bf31290ee505e7d6f
Anoop Brijeshkumar Dubey <anoopdubeytech@gmail.com>
2020-08-21 14:33:07 +0530

    second commit

--- DemoProject/src/com/demoproject/pojo/UserDetail.java
+++ DemoProject/src/com/demoproject/pojo/UserDetail.java
@@ -1,5 +1,38 @@
 package com.demoproject.pojo;
 
 public class UserDetail {
+	
+	public Integer userDetailId;
+	public String userName;
+	public String city;
+	public String email;
+	
+	public Integer getUserDetailId() {
+		return userDetailId;
+	}
+	public void setUserDetailId(Integer userDetailId) {
+		this.userDetailId = userDetailId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
 
 }
